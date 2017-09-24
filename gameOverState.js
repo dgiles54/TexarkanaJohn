44bf56eb40a66fda16f453a205a68d21eddcea30
OgimusPrime <ogimusprime@gmail.com>
2017-09-24 15:19:34 -0500

    tried

--- gameOverState.js
+++ gameOverState.js
@@ -1,7 +1,7 @@
 var gameOverState = {
     preload: function() {
         game.load.image('gameOver','assets/sprites/GameOverScreen.png');
-        game.load.spritesheet('button','assets/sprites/retryButton.png',320,168);
+        game.load.image('button','assets/sprites/retryButton.png');
     },
     
     create: function() {
@@ -9,7 +9,7 @@ var gameOverState = {
         var gameOverSprite;
         gameOverSprite = game.add.sprite(250,200,'gameOver');
         
-        button = game.add.button(game.world.centerX - 95, 100, 'button', actionOnClick, this, 2, 1, 0);
+        button = game.add.button(game.world.centerX - 95, 200, 'button', this.actionOnClick);
     },
     
     actionOnClick: function(){
