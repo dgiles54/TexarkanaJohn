f248deec7aa390fcdd3766e3e07a6af9bbaf4bd4
Dave <dgiles54@gmail.com>
2017-09-24 15:18:28 -0500

    fixed button

--- gameOverState.js
+++ gameOverState.js
@@ -1,22 +1,22 @@
 var gameOverState = {
-    preload: function() {
-        game.load.image('gameOver','assets/sprites/GameOverScreen.png');
-        game.load.spritesheet('button','assets/sprites/retryButton.png',320,168);
+    preload: function () {
+        game.load.image('gameOver', 'assets/sprites/GameOverScreen.png');
+        game.load.spritesheet('button', 'assets/sprites/retryButton.png', 320, 168);
     },
-    
-    create: function() {
+
+    create: function () {
         game.stage.backgroundColor = '#000000';
         var gameOverSprite;
-        gameOverSprite = game.add.sprite(250,200,'gameOver');
-        
-        button = game.add.button(game.world.centerX - 95, 100, 'button', actionOnClick, this, 2, 1, 0);
+        gameOverSprite = game.add.sprite(250, 200, 'gameOver');
+
+        button = game.add.button(0, 0, 'button', this.actionOnClick, this, 2, 1, 0);
     },
-    
-    actionOnClick: function(){
-    game.state.start('gameState');
-},
-    
-    update: function() {
-        
+
+    update: function () {
+
+    },
+
+    actionOnClick: function () {
+        game.state.start('gameState');
     }
 };
