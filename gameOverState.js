e7601ba89a6212f416d01d369514eeec07e94bf8
OgimusPrime <ogimusprime@gmail.com>
2017-09-24 15:56:29 -0500

    finalized retry button

--- gameOverState.js
+++ gameOverState.js
@@ -11,10 +11,28 @@ var gameOverState = {
 
         gameOverSprite = game.add.sprite(250,200,'gameOver');
         
-        button = game.add.button(game.world.centerX - 95, 200, 'button', this.actionOnClick);
+        button = game.add.button(game.world.centerX - 320, 400, 'button', this.actionOnClick);
+        button.onInputOver.add(this.over);
+        button.onInputDown.add(this.down);
+        button.onInputOut.add(this.out);
 
     },
 
+    over: function () {
+    
+    button.tint = 0x66ff66;
+},
+    
+    down: function(){
+        
+        button.tint = 0x00ff00;
+    },
+    
+    out: function(){
+        
+      button.tint = 0xFFFFFF; 
+    },
+    
     update: function () {
 
     },
