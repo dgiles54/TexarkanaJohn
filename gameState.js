2a5f4962017cc1f0c1d4a8bbc1565d5634e22d6f
Dave <dgiles54@gmail.com>
2017-10-10 18:08:25 -0500

    added lava glow

--- gameState.js
+++ gameState.js
@@ -24,11 +24,12 @@ var hasKey = false,
     blowdartCreated = false;
 var leverSound, plateSound;
 var attackAnim;
-var levelNum = 1,
+var levelNum = 4,
     maxLevels = 4;
 var snakeDirection = 'right',
     nextAttackSnake = 0;
 var LIGHT_RADIUS = 100,
+    GLOW_RADIUS = 75,
     shadowTexture;
 var playerTorch, holdingTorch = false;
 
@@ -574,7 +575,8 @@ function updateShadowTexture() {
     // Draw shadow
     shadowTexture.context.fillStyle = 'rgb(10, 10, 10)';
     shadowTexture.context.fillRect(0, 0, map.widthInPixels, map.heightInPixels);
-
+    
+    // Player torch
     // Change radius randomly each frame
     var radius = LIGHT_RADIUS + game.rnd.integerInRange(1, 10);
 
@@ -592,7 +594,8 @@ function updateShadowTexture() {
         shadowTexture.context.arc(playerTorch.x, playerTorch.y, radius, 0, Math.PI * 2);
         shadowTexture.context.fill();
     }
-
+    
+    // Torches in map
     torches.forEach(function (torch) {
         // Change radius randomly each frame
         var radius = LIGHT_RADIUS*2 + game.rnd.integerInRange(1, 10);
@@ -608,6 +611,21 @@ function updateShadowTexture() {
         shadowTexture.context.beginPath();
         shadowTexture.context.fillStyle = gradient;
         shadowTexture.context.arc(torch.x, torch.y, radius, 0, Math.PI * 2);
+        shadowTexture.context.fill();
+    });
+    
+    // Lava glow
+    map.objects['LavaGlow'].forEach(function (glowObject) {
+        var gradient =
+            shadowTexture.context.createRadialGradient(
+                glowObject.x+16, glowObject.y, GLOW_RADIUS * 0.05,
+                glowObject.x+16, glowObject.y, GLOW_RADIUS);
+        gradient.addColorStop(0, 'rgba(250, 75, 50, 1.0)');
+        gradient.addColorStop(1, 'rgba(250, 75, 50, 0.0)');
+        
+        shadowTexture.context.beginPath();
+        shadowTexture.context.fillStyle = gradient;
+        shadowTexture.context.arc(glowObject.x+16, glowObject.y, GLOW_RADIUS, 0, Math.PI * 2);
         shadowTexture.context.fill();
     });
 
