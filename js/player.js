27aab5b3845999ac4ca9b77535373f1f5461a86e
Dave <dgiles54@gmail.com>
2017-12-06 12:37:47 -0600

    sped up attack animation

--- js/player.js
+++ js/player.js
@@ -30,7 +30,7 @@ function createPlayer() {
     player.body.collideWorldBounds = true;
     // Animation
     player.animations.add('walk', [0, 1, 2, 3, 4, 5], 8, true);
-    player.attackAnimation = player.animations.add('attack', [6, 7, 8, 9, 10], 15, false);
+    player.attackAnimation = player.animations.add('attack', [6, 7, 8, 9, 10], 20, false);
     player.attackAnimation.onComplete.add(function () {
         player.frame = 0;
         player.isAttacking = false;
